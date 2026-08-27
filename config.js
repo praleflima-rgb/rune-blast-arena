3867f4938b58d537762ff7baa21bae4a394f0f80
praleflima-rgb <pr.aleflima@gmail.com>
2026-08-26 22:22:28 -0300

    Add/update config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js – external configuration for Supabase
+window.SUPABASE_URL = "https://yssdwwfqdendmfknbsgx.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_QHDV_lT-suQhvU-cFp-UXw_TGZ6EznH";
